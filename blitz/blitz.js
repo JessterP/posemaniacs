543cd0ea5d9ec49d75218a9c8d109a1ce4c635f2
Jessie <me@jessiep.me>
2020-08-25 20:09:21 -0400

    .

--- blitz/blitz.js
+++ blitz/blitz.js
@@ -5,14 +5,23 @@
     let timer = document.getElementById("timer");
     let cloak = document.getElementById("cloak");
     let skip = document.getElementById("skip");
-    let counter = 0;
 
-    function init() {
-        randomPose()
-    }
+    // settings
+    let btn10 = document.getElementById("btn10");
+    let btn15 = document.getElementById("btn15");
+    let btn30 = document.getElementById("btn30");
+    let btn40 = document.getElementById("btn40");
+    let btn60 = document.getElementById("btn60");
+    let btn90 = document.getElementById("btn90");
+    let btncd = document.getElementById("btncountdown");
+    let btnStart = document.getElementById("btnStart");
+
+    let counter = 0;
+    let pause = true;
+    let countdown = true;
 
     function randomPose() {
-        counter = -40;
+        counter = countdown?-40:0;
         let pose = poses[Math.floor(Math.random() * poses.length)];
         viewer.src = uri + pose.path + img(Math.floor(1));
     }
@@ -23,21 +32,41 @@
     }
 
     setInterval(()=>{
+        if (pause) return;
         counter++;
         if (counter < 1) cloak.classList = "t"+(-counter/10|0);
         let value = timer.value;
         if (value < 5) timer.value = value = 5;
         if (counter / 10> value) {
             randomPose();
-            counter = -40;
+            counter = countdown?-40:0;
         }
     },100);
 
     skip.onclick = randomPose;
 
+    function clearsecbtn() {
+        for (const e of document.getElementsByClassName("secbtn"))
+            e.classList.remove("selected");
+    }
+    btn10.onclick = () => {timer.value = 10;clearsecbtn(); btn10.classList.add("selected")};
+    btn15.onclick = () => {timer.value = 15;clearsecbtn(); btn15.classList.add("selected")};
+    btn30.onclick = () => {timer.value = 30;clearsecbtn(); btn30.classList.add("selected")};
+    btn40.onclick = () => {timer.value = 40;clearsecbtn(); btn40.classList.add("selected")};
+    btn60.onclick = () => {timer.value = 60;clearsecbtn(); btn60.classList.add("selected")};
+    btn90.onclick = () => {timer.value = 90;clearsecbtn(); btn90.classList.add("selected")};
+
+    btncd.onclick = () => {countdown=!countdown; btncd.innerText = countdown?"On":"Off"};
+
+    btnStart.onclick = () => {
+        pause = false;
+        document.getElementById("main-viewer").classList.remove("hidden");
+        document.getElementById("settings").classList.add("hidden");
+        randomPose();
+    }
+
     $.getJSON('../data.json', function(data) {
         poses = data;
-        init();
         $("#loading").remove();
     });
 })();
